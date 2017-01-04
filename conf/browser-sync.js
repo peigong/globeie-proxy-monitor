f1977298eb54af7d9fb96ae07cfdeeb64a948196
周培公 <peigong@foxmail.com>
2017-01-04 18:20:42 +0800

    stub

--- conf/browser-sync.js
+++ conf/browser-sync.js
@@ -1,11 +1,31 @@
+import { readFile } from 'fs';
+import { join } from 'path';
+import logger from 'connect-logger';
 
 const dist = 'dist/monitor';
+const server = 'dist/server';
 
 export default {
 	server: {
-		baseDir: [ dist ],
+		baseDir: [ dist, server ],
         index: 'index.html'
 	},
 	middleware: [
+        logger(),
+        function(req, res, next){
+            let url = req.url;
+            let reg = /php$/;
+            if(reg.test(url)){
+                let filename = join(__dirname, '..', 'stub', url.replace(reg, 'json'));
+                readFile(filename, function(err, data){
+                    if(err){
+                        data = err.message;
+                    }
+                    res.end(data);
+                });
+            }else{
+                next();
+            }
+        }
 	]
 }
