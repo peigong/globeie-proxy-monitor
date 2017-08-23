377fb7ba47e4e0b0c476e285e082fb693fceae34
周培公 <peigong@foxmail.com>
2017-08-23 20:36:48 +0800

    ok

--- src/es6/att/reducer.js
+++ src/es6/att/reducer.js
@@ -2,18 +2,17 @@ import { ATTENDANCE_FETCH } from './action.js';
 
 export default function(state = {}, action){
     if(ATTENDANCE_FETCH === action.type){
+        let data = '', name = '', number = '', datetime = '', arr = [], time = '', date = '';
         if(action.response){
-            let data = action.response;
-            let name = data.name || '';
-            let number = data.number || '';
-            let datetime = data.time || '';
-            let arr = datetime.split(' ');
-            let time = arr.pop();
-            let date = arr.pop();
-            return { name, number, date, time };
-        }else{
-            return null;
+            data = action.response;
+            name = data.name || '';
+            number = data.number || '';
+            datetime = data.time || '';
+            arr = datetime.split(' ');
+            time = arr.pop();
+            date = arr.pop();
         }
+        return { name, number, date, time };
     }
     return state;
 };
